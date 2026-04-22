6149f8bff7b5187ffd5f9ca805f7f6b0d8255a37
KimJayhyun <dnlwjem000@gmail.com>
2026-04-22 21:19:24 +0900

    Creational: Finished simple factory pattern

--- src/com/coffeepoweredcrew/simplefactory/Client.java
+++ src/com/coffeepoweredcrew/simplefactory/Client.java
@@ -3,7 +3,9 @@ package com.coffeepoweredcrew.simplefactory;
 public class Client {
 
 	public static void main(String[] args) {
-		
+		Post post = PostFactory.createPost("blog");
+		System.out.println(post);
+
 
 	}
 
